b69cc5540e9e291bf33abb52afd52d920b71e06d
toot-a-loot <mjartgithub@gmail.com>
2025-07-07 14:27:08 +0800

    dashboard testing

---
+++ bathroom_app/lib/Dashboard/main_dashboard_map.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:bathroom_app/Dashboard/main_dashboard_searchArea.dart'; 
+
+class DashboardMap extends StatefulWidget {
+  const DashboardMap({super.key});
+
+  @override
+  State<DashboardMap> createState() => _DashboardMapState();
+}
+
+class _DashboardMapState extends State<DashboardMap> {
+  static const LatLng tempLocation = LatLng(10.3280, 123.9063);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Google Map
+          Positioned.fill(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: tempLocation,
+                zoom: 16,
+              ),
+              zoomControlsEnabled: false,
+              zoomGesturesEnabled: true,
+              myLocationButtonEnabled: false,
+              mapToolbarEnabled: false,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
