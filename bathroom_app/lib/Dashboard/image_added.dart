c22ed6052ebccd6d8f65f36e7582cdbee8e6775c
toot-a-loot <mjartgithub@gmail.com>
2025-11-16 23:24:37 +0800

    Chore : Implemented Signup and Login

--- bathroom_app/lib/Dashboard/image_added.dart
+++
@@ -1,4 +0,0 @@
-/// Compatibility stub exporting the centralized [ImageAddedWidget].
-///
-/// Keep this file during migration to avoid changing existing imports.
-export 'package:bathroom_app/widgets/image_added.dart';
